b08f7aaef671ea7da14a4dbd7c6de23b61db9628
kaanlab <kaan@live.ru>
2021-04-27 16:53:16 +0300

    add test for PassFactory class

---
+++ PassKeeper.PassLib.Tests/PassFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PassKeeper.PassLib.Tests
+{
+    public class PassFactoryTests
+    {        
+        [Fact]
+        public void GenerateOnePassword_TakeRandomFromTwoDigitsFourCharsFromTwoWords_ReturnDictionaryWithPasswordAndPasswordPhrase()
+        {
+            var passwordCount = 1;
+            var charsInPassword = 4;
+            var adjeciveWords = new[] { "канониров", "глыб" };
+            var nounsWors = new[] { "абажуров", "козликов" };
+ 
+            var result = PassFactory.Create(passwordCount, charsInPassword, adjeciveWords, nounsWors);
+
+            result.Should().HaveCount(1);
+        }
+    }
+}
