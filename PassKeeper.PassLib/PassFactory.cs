7530a6ed0784ffaa36e65f5a27f84fda3d350000
kaanlab <kaan@live.ru>
2021-04-29 17:12:23 +0300

    create factory method for password

--- PassKeeper.PassLib/PassFactory.cs
+++ PassKeeper.PassLib/PassFactory.cs
@@ -13,16 +13,16 @@ namespace PassKeeper.PassLib
         {
             var passwords = new Dictionary<string, string>();
             var random = new Random();
-            var strigHelper = new StringHelper();
+            var strigHelper = new Vocabulary();
 
-            for (int i = 0; i < count; i++)
-            {
-                var randomInt = IntRange.empty == digits ? 0 : GetRandomInt(digits, random);
-                var randomAdjectiveWord = GetRandomWord(adjectivesWords, random);
-                var randomNounWord = GetRandomWord(nounsWords, random);
-                var result = strigHelper.CreatePassAndPhrase(charsInPass, randomAdjectiveWord, randomNounWord, randomInt);
-                passwords.Add(result.password, result.phrase);
-            }
+            //for (int i = 0; i < count; i++)
+            //{
+            //    var randomInt = IntRange.empty == digits ? 0 : GetRandomInt(digits, random);
+            //    var randomAdjectiveWord = GetRandomWord(adjectivesWords, random);
+            //    var randomNounWord = GetRandomWord(nounsWords, random);
+            //    var result = strigHelper.CreatePassAndPhrase(charsInPass, randomAdjectiveWord, randomNounWord, randomInt);
+            //    passwords.TryAdd(result.password, result.phrase);
+            //}
 
             return passwords;
         }
@@ -34,5 +34,38 @@ namespace PassKeeper.PassLib
         }
 
         private static int GetRandomInt(IntRange maxInt, Random random) => random.Next((int)maxInt);
+
+        public static (string password, string phrase) CreatePassAndPhrase(int charsInPass, string adjectiveWord, string nounWord, int digitsInPass = 0)
+        {
+            var firtsPasswordWord = !string.IsNullOrEmpty(adjectiveWord) ? adjectiveWord.FirstCharUppercase() : "";
+            var firstPasswordPart = ReplaceChars(firtsPasswordWord, charsInPass);
+            var secondPasswordWord = !string.IsNullOrEmpty(nounWord) ? nounWord.FirstCharUppercase() : "";
+            var secondPasswordPart = ReplaceChars(secondPasswordWord, charsInPass);
+
+            var password = digitsInPass == 0 ? $"{firstPasswordPart}{ secondPasswordPart}" : $"{digitsInPass}{ firstPasswordPart}{ secondPasswordPart}";
+            var phrase = digitsInPass == 0 ? $"{firtsPasswordWord} {secondPasswordWord}" : $"{digitsInPass} {firtsPasswordWord} {secondPasswordWord}";
+
+            return (password, phrase);
+
+        }
+
+        private static string ReplaceChars(string inputWord, int wordLenth)
+        {
+            var rusKey = "Ё!\"№;%:?*()_+ЙЦУКЕНГШЩЗХЪ/ФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю. ".ToArray();
+            var engKey = "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./ ".ToArray();
+
+            var substring = inputWord.Substring(0, wordLenth);
+            var sub = substring.ToCharArray();
+
+            var outString = new StringBuilder();
+            for (int i = 0; i < sub.Length; i++)
+            {
+                var rusIndex = sub[i];
+                var index = Array.IndexOf(rusKey, rusIndex);
+                outString.Append(engKey[index]);
+            }
+
+            return outString.ToString();
+        }
     }
 }
