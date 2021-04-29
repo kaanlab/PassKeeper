c4c8addd03af5e4e3c7cc2e2f795f7bf1a0e0ad5
kaanlab <kaan@live.ru>
2021-04-29 17:11:16 +0300

    add new extention metod for string

--- PassKeeper.PassLib/ExtentionsMethods.cs
+++ PassKeeper.PassLib/ExtentionsMethods.cs
@@ -1,14 +1,32 @@
 ﻿using DeepMorphy.Model;
-
+using System.IO;
 
 namespace PassKeeper.PassLib
 {
     public static class ExtentionsMethods
     {
-        public static bool IsNoun(this MorphInfo morphInfo) => (morphInfo["чр"].BestGramKey == "сущ");
+        public static bool IsNoun(this MorphInfo morphInfo) => morphInfo["чр"].BestGramKey == "сущ";
 
-        public static bool IsAjective(this MorphInfo morphInfo) => (morphInfo["чр"].BestGramKey == "прил");
+        public static bool IsAjective(this MorphInfo morphInfo) => morphInfo["чр"].BestGramKey == "прил";
 
         public static string FirstCharUppercase(this string s) =>  char.ToUpper(s[0]) + s.Substring(1);
+
+
+        public static void AppendTextToFile(this string path, string text)
+        {
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(text);
+            }
+        }
     }
 }
