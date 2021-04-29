aba554f87672543b42cb96f3d92b8700079150d6
kaanlab <kaan@live.ru>
2021-04-29 17:09:44 +0300

    rename class

---
+++ PassKeeper.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using PassKeeper.PassLib;
+using System;
+using System.IO;
+
+namespace PassKeeper.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nounWords = File.ReadAllLines("nouns.txt");
+            var adjectiveWords = File.ReadAllLines("ajectives.txt");
+
+            var vocaboulary = new Vocabulary();
+
+            var ajectivesOutputFile = "ajectives_only.txt";
+            foreach (var word in adjectiveWords)
+            {
+                if (vocaboulary.IsAjective(word))
+                    ajectivesOutputFile.AppendTextToFile(word);
+            }
+
+            var nounsOutputFile = "nouns_only.txt";
+            foreach (var word in nounWords)
+            {
+                if (vocaboulary.IsNoun(word))
+                    nounsOutputFile.AppendTextToFile(word);
+            }
+
+        }
+    }
+}
---
+++ PassKeeper.PassLib/Fb2Parser.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Xml;
+
+
+namespace PassKeeper.PassLib
+{
+    public class Fb2Parser
+    {
+        public void Parse(string inputFilePath, string outputFilePath)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(inputFilePath);
+
+            XmlNodeList elemList = xDoc.GetElementsByTagName("strong");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(elemList[i].InnerXml))
+                    outputFilePath.AppendTextToFile(elemList[i].InnerXml);
+            }
+        }       
+    }
+}
